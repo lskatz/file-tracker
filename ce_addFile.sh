8285322442de164eb3b78499ea61c08db697238f
Lee Katz - Aspen <gzu2@cdc.gov>
2018-10-02 08:43:39 -0400

    allowing multiple files to be added at once

--- ce_addFile.sh
+++ ce_addFile.sh
@@ -2,72 +2,70 @@
 
 set -e
 
-db=$1
-path=$2
+db=$1; shift
+paths=$@
 
 set -u
 
-if [ "$path" == "" ]; then
+if [ "$paths" == "" ]; then
   echo "Adds a file to the CE database. The file should already be in the CE."
-  echo "Usage: $0 database.sqlite path/to/file.fastq.gz"
+  echo "Usage: $0 database.sqlite path/to/file.fastq.gz [second/path/file.fastq.gz...]"
   exit 0
 fi
 
-path=$(realpath $path)
+# Make all directories first to help with directory foreign
+# key requirements.
+for path in $paths; do
+  path=$(realpath $path)
+  dirname=$(dirname $path)
 
-filename=$(basename $path)
-dirname=$(dirname $path)
-filesize=$(wc -c < "$path")
-md5sum=$(md5sum < "$path" | awk '{print $1}')
+  # Directory table
+  dir_id=$(ce_addDir.sh "$db" "$dirname");
+done
 
-# Directory table
-dir_id=$(
-  sqlite3 "$db" "
-    SELECT dir_id
-    FROM DIRECTORY
-    WHERE path='$dirname';
-  "
-)
-if [ "$dir_id" == "" ]; then
-  dir_id=$(
+
+for path in $paths; do
+  path=$(realpath $path)
+
+  filename=$(basename $path)
+  dirname=$(dirname $path)
+  filesize=$(wc -c < "$path")
+  md5sum=$(md5sum < "$path" | awk '{print $1}')
+
+  # Directory table
+  #dir_id=$(ce_addDir.sh "$db" "$dirname");
+
+  # File table
+  # TODO add in a first set of values with /dev/null or some comment from a cli parameter
+  file_id=$(
     sqlite3 "$db" "
       PRAGMA foreign_keys=ON;
-      INSERT INTO DIRECTORY(path)
-        VALUES ('$dirname');
+
+      INSERT INTO FILE(filename, dir_id, filesize, md5sum)
+        VALUES ('$filename', 
+        (SELECT dir_id FROM DIRECTORY WHERE path='$dirname'),
+        '$filesize', '$md5sum');
 
       SELECT last_insert_rowid();
     "
   )
-fi
 
-# File table
-# TODO add in a first set of values with /dev/null or some comment from a cli parameter
-file_id=$(
-  sqlite3 "$db" "
-    PRAGMA foreign_keys=ON;
+  # File operation table
+  op_id=$(
+    sqlite3 "$db" "
+      PRAGMA foreign_keys=ON;
+      INSERT INTO OPERATION(file_id, date, time, operation, to_name, to_dir)
+        VALUES ($file_id, 
+          (SELECT DATE('now','localtime')), 
+          (SELECT TIME('now','localtime')),
+          (SELECT op_enum_id FROM OPERATION_ENUM WHERE operation='init'),
+          '$filename', $dir_id
+        );
+        SELECT last_insert_rowid();
+      "
+    )
 
-    INSERT INTO FILE(filename, dir_id, filesize, md5sum)
-      VALUES ('$filename', '$dir_id', '$filesize', '$md5sum');
+  echo "Initialized file $path into $db" >&2
+  echo "$op_id"
 
-    SELECT last_insert_rowid();
-  "
-)
-
-# File operation table
-op_id=$(
-  sqlite3 "$db" "
-    PRAGMA foreign_keys=ON;
-    INSERT INTO OPERATION(file_id, date, time, operation, to_name, to_dir)
-      VALUES ($file_id, 
-        (SELECT DATE('now','localtime')), 
-        (SELECT TIME('now','localtime')),
-        (SELECT op_enum_id FROM OPERATION_ENUM WHERE operation='init'),
-        '$filename', $dir_id
-      );
-      SELECT last_insert_rowid();
-    "
-  )
-
-echo "Initialized file $path into $db" >&2
-echo "$op_id"
-
+done
